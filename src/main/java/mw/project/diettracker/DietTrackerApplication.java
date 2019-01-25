ad52fec07326a4d8b9c429da7aaee3a365e4185b
mwisnie <metastable321@gmail.com>
2019-01-25 20:10:13 +0100

    initial project commit

---
+++ src/main/java/mw/project/diettracker/DietTrackerApplication.java
@@ -0,0 +1,14 @@
+package mw.project.diettracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DietTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DietTrackerApplication.class, args);
+	}
+
+}
+
